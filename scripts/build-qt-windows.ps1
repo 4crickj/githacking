5fd38ed62666b7cabd2c6665feecc51252bfc8db
zcz3 <zcz3@pm.me>
2020-08-08 23:32:16 +0100

    Update build-qt-windows.ps1

--- scripts/build-qt-windows.ps1
+++ scripts/build-qt-windows.ps1
@@ -3,18 +3,20 @@ $qtCore = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submo
 $qtCoreFolder = "qtbase-everywhere-src-5.15.0"
 $qtMultimedia = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submodules/qtmultimedia-everywhere-src-5.15.0.zip"
 $qtMultimediaFolder = "qtmultimedia-everywhere-src-5.15.0"
-$qtInstall = "C:\Qt\Qt-5.15.0"
+$qtBuild = "C:\QtBuild\
+$qtInstall = "C:\QtStatic\"
 
 $env:Path += ";$qtInstall\qtbase\bin;$qtInstall\gnuwin32\bin"
 
 Invoke-WebRequest -Uri $qtCore -OutFile qtCore.zip
 7z x qtCore.zip -oqtCore
+mv "qtCore\$qtCoreFolder" "$qtBuild"
 
 Invoke-WebRequest -Uri $qtMultimedia -OutFile qtMultimedia.zip
 7z x qtMultiMedia.zip -oqtMultimedia
-mv "qtMultimedia\$qtMultimediaFolder" "qtCore\$qtCoreFolder\src\multimedia"
+mv "qtMultimedia\$qtMultimediaFolder" "$qtBuild\src\multimedia"
 
-cd "qtCore\$qtCoreFolder"
+cd "$qtBuild"
 
 gcc -v
 
@@ -23,9 +25,9 @@ gcc -v
 #exit
 
 ./configure `
-  -opensource -confirm-license -release -static `
+  -opensource -confirm-license -release -static -prefix $qtInstall`
   -make libs `
-  -no-ssl -no-opengl -no-angle -no-direct2d -no-gif -no-ico -no-libpng -no-libjpeg
+  -no-ssl -no-opengl -no-angle -no-direct2d -no-gif -no-ico -no-libpng -no-libjpeg 
 
 mingw32-make -j4
 mingw32-make install
