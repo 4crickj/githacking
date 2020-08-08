41a0eb463d3dc1e88b76b005837ffcf4460a04fd
zcz3 <zcz3@pm.me>
2020-08-08 22:36:39 +0100

    Update build-qt-windows.ps1

--- scripts/build-qt-windows.ps1
+++ scripts/build-qt-windows.ps1
@@ -3,6 +3,9 @@ $qtCore = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submo
 $qtCoreFolder = "qtbase-everywhere-src-5.15.0"
 $qtMultimedia = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submodules/qtmultimedia-everywhere-src-5.15.0.zip"
 $qtMultimediaFolder = "qtmultimedia-everywhere-src-5.15.0"
+$qtInstall = "C:\Qt\Qt-5.15.0"
+
+$env:Path += ";$qtInstall\qtbase\bin;$qtInstall\gnuwin32\bin"
 
 Invoke-WebRequest -Uri $qtCore -OutFile qtCore.zip
 7z x qtCore.zip -oqtCore
@@ -15,19 +18,17 @@ cd "qtCore\$qtCoreFolder"
 
 gcc -v
 
-./configure --help
-./configure --list-features
-
-exit
+#./configure --help
+#./configure --list-features
+#exit
 
 ./configure `
-  -opensource -confirm-license -release -disable-shared -static `
+  -opensource -confirm-license -release -static `
   -make libs `
   -no-ssl -no-opengl -no-angle -no-direct2d -no-gif -no-ico -no-libpng -no-libjpeg
 
 mingw32-make -j4
 mingw32-make install
 
-$qtInstall = "C:\Qt\Qt-5.15.0"
 dir $qtInstall
  
