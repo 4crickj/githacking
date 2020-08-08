908a859740f4b6b3f744eae32c33c717cee1bc1a
zcz3 <zcz3@pm.me>
2020-08-08 19:37:03 +0100

    Update build-qt-windows.ps1

--- scripts/build-qt-windows.ps1
+++ scripts/build-qt-windows.ps1
@@ -1,6 +1,9 @@
 
-$qtSource = "http://download.qt-project.org/official_releases/qt/5.12/5.12.9/submodules/qtbase-everywhere-src-5.12.9.zip"
+$qtSource = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submodules/qtbase-everywhere-src-5.15.0.zip"
+$qtSourceFolder = "qtbase-everywhere-src-5.15.0"
 
 Invoke-WebRequest -Uri $qtSource -OutFile qt_source.zip
 
 7z x qt_source.zip -oqt_source
+
+qt_source/$qt_sourceFolder/configure --help
