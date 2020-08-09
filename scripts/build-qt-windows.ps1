aa7df37c884e6e1e4d0c3cc62f2734e029e15506
zcz3 <zcz3@pm.me>
2020-08-10 00:30:04 +0100

    Update build-qt-windows.ps1

--- scripts/build-qt-windows.ps1
+++ scripts/build-qt-windows.ps1
@@ -1,14 +1,20 @@
 
-$qtMulti = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/single/qt-everywhere-src-5.15.0.zip"
-$qtMultiFolder = "qt-everywhere-src-5.15.0"
+$qtCore = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submodules/qtbase-everywhere-src-5.15.0.zip"
+$qtCoreFolder = "qtbase-everywhere-src-5.15.0"
+$qtMultimedia = "http://download.qt-project.org/official_releases/qt/5.15/5.15.0/submodules/qtmultimedia-everywhere-src-5.15.0.zip"
+$qtMultimediaFolder = "qtmultimedia-everywhere-src-5.15.0"
 $qtBuild = "C:\QtBuild\"
 $qtInstall = "C:\QtStatic\"
 
 $env:Path += ";$qtInstall\qtbase\bin;$qtInstall\gnuwin32\bin"
 
-Invoke-WebRequest -Uri $qtMulti -OutFile qtMulti.zip
-7z x qtMulti.zip -oqtMulti
-mv "qtMulti\$qtMultiFolder" "$qtBuild"
+Invoke-WebRequest -Uri $qtCore -OutFile qtCore.zip
+7z x qtCore.zip -oqtCore
+mv "qtCore\$qtCoreFolder" "$qtBuild"
+
+Invoke-WebRequest -Uri $qtMultimedia -OutFile qtMultimedia.zip
+7z x qtMultiMedia.zip -oqtMultimedia
+mv "qtMultimedia\$qtMultimediaFolder" "$qtBuild\multimedia"
 
 cd "$qtBuild"
 
