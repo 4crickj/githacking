c110d7881fbb2bb9add26aaa6b4669a9dc77a81b
zcz3 <zcz3@pm.me>
2020-08-16 17:45:30 +0100

    Update build-libltc-windows.sh

--- scripts/build-libltc-windows.sh
+++ scripts/build-libltc-windows.sh
@@ -2,9 +2,17 @@
 
 set -e -u
 
-wget https://github.com/x42/libltc/releases/download/v1.3.1/libltc-1.3.1.tar.gz
-tar -xf libltc-1.3.1.tar.gz
+libltc_ver=libltc-1.3.1
 
-ls -l
+wget https://github.com/x42/libltc/releases/download/v1.3.1/${libltc_ver}.tar.gz
+tar -xf ${libltc_ver}.tar.gz
+mv ${libltc_ver} libltc
+
+ls -l libltc
+
+cd libltc
+./configure --help
+
+
 
 
